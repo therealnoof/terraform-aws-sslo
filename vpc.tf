214dd2803034dad5f581f4cfa7264ddb3fbd48ef
noofdog <noof@DESKTOP-MT293TI.localdomain>
2021-09-15 12:57:28 -0400

    update

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
-# Create the VPC
+# Create the VPC's
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name                 = "terraform-aws-sslo"
+  name                 = "terraform-aws-sslo-securitystack"
   cidr                 = "10.0.0.0/16"
   azs                  = [var.az]
   enable_nat_gateway   = "true"
